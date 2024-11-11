acfde9bff43553776c3dbd34875882935940501c
Nicolás Palacio <163795580+nico-palacio@users.noreply.github.com>
2024-11-11 16:00:34 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function link_RDRII() {
+    window.location.href = "https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2?snr=1_7_15__13";
+}
+
+function link_WOT() {
+    window.location.href = "https://store.steampowered.com/app/1407200/World_of_Tanks/";
+}
+
+function link_DSR() {
+    window.location.href = "https://store.steampowered.com/app/570940/DARK_SOULS_REMASTERED/";
+}
