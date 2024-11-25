35ee06e588b054a4a2ba61c5a02807933fee0ee9
Nicolás Palacio <163795580+nico-palacio@users.noreply.github.com>
2024-11-25 16:33:17 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,41 @@ function link_WOT() {
 function link_DSR() {
     window.location.href = "https://store.steampowered.com/app/570940/DARK_SOULS_REMASTERED/";
 }
+
+function link_CS2() {
+    window.location.href = "https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/";
+}
+
+function link_FF14() {
+    window.location.href = "https://store.steampowered.com/app/39210/FINAL_FANTASY_XIV_Online/";
+}
+
+function link_F124() {
+    window.location.href = "https://store.steampowered.com/app/2488620/F1_24/";
+}
+
+function link_VS() {
+    window.location.href = "https://store.steampowered.com/app/1794680/Vampire_Survivors/";
+}
+function link_HK() {
+    window.location.href = "https://store.steampowered.com/app/367520/Hollow_Knight/";
+}
+
+function link_SV() {
+    window.location.href = "https://store.steampowered.com/app/413150/Stardew_Valley/";
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    console.log("Nombre:", name);
+    console.log("Email:", email);
+    console.log("Mensaje:", message);
+
+    alert("¡Se ha enviado tu formulario correctamente!");
+});
